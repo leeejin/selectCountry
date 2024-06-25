8546255fb42ae5612529578a9fd6a876c75fe546
leeejin <asd72621@naver.com>
2024-06-25 15:35:37 +0900

    Chore: clsx 라이브러리 설치

--- src/components/CountryCard/CountryCard.tsx
+++ src/components/CountryCard/CountryCard.tsx
@@ -1,9 +1,17 @@
+import clsx from "clsx";
 import { PropsWithChildren } from "react";
 import { SelectCard } from "../../types/type";
 
 function CountryCard({ handleSelect, country }: PropsWithChildren<SelectCard>) {
+  console.log(country);
   return (
-    <div className="card" onClick={() => handleSelect(country)}>
+    <div
+      className={clsx("card", {
+        "border-green-500": country.select,
+        "border-none": !country.select,
+      })}
+      onClick={() => handleSelect(country)}
+    >
       <img className="w-[60px] m-auto" src={country.flags} />
       <h4 className="font-semibold">{country.name}</h4>
       <p className="text-gray-600">{country.capital}</p>
