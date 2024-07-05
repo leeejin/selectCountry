bc250bf1b7361f8949bbb4df294603d53d4ef612
leeejin <asd72621@naver.com>
2024-07-05 15:42:03 +0900

    Fix: 페이지네이션 피드백 수정

--- src/api/country.api.ts
+++ src/api/country.api.ts
@@ -1,43 +1,62 @@
 import { AxiosInstance } from "axios";
 import { itemCountPerPage } from "../components/CountryList/CountryList";
-import { Country } from "../types/type";
+import { APICountry } from "../types/type";
 
 class CountryAPI {
   private client: AxiosInstance;
-  private index: number = 0;
+
   constructor(client: AxiosInstance) {
     this.client = client;
   }
-  async getAllCountries(offset: number) {
+
+  async getCountriesByPage(page: number) {
+    const allCountries = await this.getAllCountries();
+    const start = page * itemCountPerPage;
+    const end = itemCountPerPage * (page + 1);
+    const countriesByPage = allCountries.slice(start, end);
+
+    return countriesByPage;
+  }
+
+  async getAllCountries() {
     try {
-      const response = await this.client.get("/all");
-      const data = response.data;
-      const total_pages: number = Math.ceil(data.length / itemCountPerPage);
+      const response = await this.client.get<APICountry[]>("/all");
+      const countries = response.data.map((country, index: number) => ({
+        id: index,
+        capital: country.capital || [""],
+        name: country.name.common,
+        flags: country.flags.png,
+        select: false,
+      }));
 
-      const results: Country[] = Array.from(
-        { length: offset + itemCountPerPage },
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        (_) => {
-          const item = data[this.index];
-          if (!item) {
-            return null;
-          }
-
-          const result: Country = {
-            id: this.index,
-            capital: item.capital || [""],
-            name: item.name.common,
-            flags: item.flags.png,
-            select: false,
-          };
-          this.index++;
-          return result;
-        }
-      ).filter((item) => item !== null);
-      return { results, total_pages };
+      return countries;
     } catch (error) {
-      console.error(error);
+      return [];
     }
   }
 }
 export default CountryAPI;
+//  const data = response.data;
+//  const total_pages: number = Math.ceil(data.length / itemCountPerPage);
+//  const queryLength = page === 0 ? itemCountPerPage : response.data.length;
+// const results: Country[] = Array.from(
+//   { length: queryLength },
+//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//   (_) => {
+//     const item = data[this.index];
+//     if (!item) {
+//       return null;
+//     }
+
+//     const result: Country = {
+//       id: this.index,
+//       capital: item.capital || [""],
+//       name: item.name.common,
+//       flags: item.flags.png,
+//       select: false,
+//     };
+//     this.index++;
+//     return result;
+//   }
+// ).filter((item) => item !== null);
+// return { results, total_pages };
--- src/components/CountryList/CountryList.tsx
+++ src/components/CountryList/CountryList.tsx
@@ -1,3 +1,4 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import api from "../../api/api";
 import { Country } from "../../types/type";
@@ -8,85 +9,80 @@ export const itemCountPerPage: number = 20;
 export const pageCountPerPage: number = 10;
 
 function CountryList() {
-  const [totalCountries, setTotalCountries] = useState<Country[]>([]);
-  const [totalLength, setTotalLength] = useState<number>(0);
-  const [offset, setOffset] = useState<number>(0);
+  const queryClient = useQueryClient();
+  const [page, setPage] = useState<number>(0);
+  const [selectedCountries, setSelectedCountries] = useState<Country[]>([]);
+
+  const { data: countriesByPage = [] } = useQuery({
+    queryKey: ["countries", { page }],
+    queryFn: ({ queryKey }) =>
+      api.countries.getCountriesByPage((queryKey[1] as { page: number }).page),
+  });
+  const { data: allCountries = [] } = useQuery({
+    queryKey: ["countries"],
+    queryFn: () => api.countries.getAllCountries(),
+  });
+
   useEffect(() => {
-    async function fetchCountries() {
-      const response = await api.countries.getAllCountries(offset);
-      if (response) {
-        const {
-          results,
-          total_pages,
-        }: { results: Country[]; total_pages: number } = response;
-        console.log(results, total_pages);
-        setTotalCountries((prev) => prev.concat(results));
-        setTotalLength(total_pages);
-      }
-    }
-    fetchCountries();
-  }, [offset]);
+    queryClient.prefetchQuery({
+      queryKey: ["countries", { page: page + 1 }],
+      queryFn: ({ queryKey }) =>
+        api.countries.getCountriesByPage(
+          (queryKey[1] as { page: number }).page
+        ),
+    });
+  }, [page, queryClient]);
 
   const handleSelect = (selectedCountry: Country): void => {
-    const updatedCountries = totalCountries.map((country) =>
-      country.id === selectedCountry.id
-        ? { ...country, select: !country.select }
-        : country
-    );
-    setTotalCountries(updatedCountries);
+    setSelectedCountries((prev) => [...prev, selectedCountry]);
   };
-  const handleSort = (): void => {
-    const sortedCountries = [...totalCountries].sort((a, b) =>
+  const handleDelete = (selectedCountry: Country): void => {
+    const filteredSelectedCountries = selectedCountries.filter(
+      (country) => country.id !== selectedCountry.id
+    );
+    setSelectedCountries(filteredSelectedCountries);
+  };
+  const handleSort = () => {
+    const filteredCountries = [...selectedCountries].sort((a, b) =>
       a.name.localeCompare(b.name)
     );
-    setTotalCountries(sortedCountries);
+
+    setSelectedCountries(filteredCountries);
   };
-  const setCurrentPageFunc = (page: number): void => {
-    const lastOffset: number = (page - 1) * itemCountPerPage;
-    setOffset(lastOffset);
-  };
+
   return (
     <div className="flex flex-col gap-4 w-3/4 mx-auto p-6">
       <h2 className="font-title text-lg mt-12">Favorite Countries</h2>
-
-      <div className="card-box">
-        {totalCountries?.map((selectedCountry) => {
-          if (selectedCountry.select)
-            return (
-              <CountryCard
-                key={selectedCountry.id}
-                country={selectedCountry}
-                handleSelect={() => handleSelect(selectedCountry)}
-              />
-            );
-        })}
-      </div>
       <div>
         <button onClick={handleSort}>이름순</button>
+      </div>
+      <div className="card-box">
+        {selectedCountries.map((selectedCountry) => (
+          <CountryCard
+            key={selectedCountry.id}
+            country={selectedCountry}
+            handleSelect={() => handleDelete(selectedCountry)}
+          />
+        ))}
       </div>
 
       <h2 className="font-title text-2xl">Countries</h2>
       <div className="card-box">
-        {totalCountries
-          ?.slice(offset, offset + itemCountPerPage)
-          .map((country) => {
-            if (!country.select)
-              return (
-                <CountryCard
-                  key={country.id}
-                  country={country}
-                  handleSelect={() => handleSelect(country)}
-                />
-              );
-          })}
+        {countriesByPage.map((country) => (
+          <CountryCard
+            key={country.id}
+            country={country}
+            handleSelect={() => handleSelect(country)}
+          />
+        ))}
       </div>
       <div className="text-center">
-        {totalCountries.length > 0 && (
+        {allCountries.length > 0 && (
           <Pagination
-            maxPage={totalLength}
+            maxPage={Math.ceil(allCountries.length / itemCountPerPage)}
             itemCountPerPage={itemCountPerPage}
             pageCountPerPage={pageCountPerPage}
-            clickListener={setCurrentPageFunc}
+            clickListener={(page) => setPage(page - 1)}
           />
         )}
       </div>
